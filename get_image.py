7394c45b69122932128719cc88a44eaf454f5d95
arvinxian <1534001170@qq.com>
2022-02-25 14:49:00 +0800

    firt version

---
+++ get_image.py
@@ -0,0 +1,17 @@
+import json
+import requests
+from requests.packages import urllib3
+import urllib3
+
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+urllib3.disable_warnings()
+# word = 'devil'
+word = input("Enter your word:\n ")
+req = requests.get('https://picdict.youdao.com/search?q=' + word + '&le=en',verify=False)
+# resultContent = json.dumps(req.content)
+resultContent = req.json()
+imageList = resultContent["data"]["pic"]
+if imageList:
+    print("图片地址为",imageList[0]["image"])
+# print(req.text)
